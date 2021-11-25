5de205fa4b39cf9b785e3d9e0bfa8f0f36c39309
G.Vladislava251 <G.Vladislava251@gmail.com>
2021-11-25 19:45:46 +0200

    Theme: Redux

--- src/redux/dialogs-reducer.jsx
+++ src/redux/dialogs-reducer.jsx
@@ -2,7 +2,26 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        { id: 1, name: "Dimych" },
+        { id: 2, name: "Andrew" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Victor" },
+        { id: 6, name: "Valera" },
+    ],
+    messages: [
+        { id: 1, message: "Hi" },
+        { id: 1, message: "How is your IT-Kamasutra?" },
+        { id: 1, message: "Yo" },
+        { id: 1, message: "Yo" },
+        { id: 1, message: "Yo" },
+    ],
+    newMessageBody: '',
+};
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
@@ -16,7 +35,7 @@ const dialogsReducer = (state, action) => {
     return state;
 };
 
-export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body});
-export const sendMessageCreator = () => ({type: SEND_MESSAGE});
+export const updateNewMessageBodyCreator = (body) => ({ type: UPDATE_NEW_MESSAGE_BODY, body: body });
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE });
 
 export default dialogsReducer;
